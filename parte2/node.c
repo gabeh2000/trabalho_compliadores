4a3984c9799aa641c7754bf63bdb2fa3e63bea39
GabrielH <gabihentschke@gmail.com>
2023-04-28 13:12:09 -0300

    parte dois do trabalho, ainda incompleta. Parte da arvore pronta, acho

---
+++ parte2/node.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdarg.h>
+#include "node.h"
+
+
+Node * syntax_tree;
+
+Node* create_node(int nl, Node_type t, char* lexeme,  /* Node* children */ ...)
+{
+	
+	Node* new_node = (Node *)malloc(sizeof(Node));
+
+	//Code ...
+    va_list ap;
+    va_start(ap, lexeme);
+
+    int n_args = 0;
+    const Node* arg = va_arg(ap, const Node*);
+    for (;  arg != NULL;  arg = va_arg(ap, const Node*)) {
+        printf("Recebi um '%s'\n", arg);
+        n_args ++;
+    }
+	new_node->n_child = n_args;
+    va_end(ap);
+    va_start(valist, lexeme);
+
+    new_node->children = (Node**)malloc(sizeof(Node*)*n_child);
+
+    for(int i = 0; i < n_child; i++){
+        Node *aux = va_arg(valist, Node *);
+        //aux->dad = new_node;
+        new_node->children[i] = aux;
+    }
+
+    va_end(valist);
+
+	return new_node;
+}
+
+int nb_of_children(Node* n)
+{
+	assert(n != NULL);
+	return n->n_child;
+}
+
+int is_leaf(Node* n)
+{
+	assert(n != NULL);
+	return n->n_child == 0;
+}
+
+Node* child(Node* n, int i)
+{
+	assert(n != NULL);
+	assert((0 <= i) && ((unsigned int)i < n->n_child));
+	return n->children[i];
+}
+
+int deep_free_node(Node* n)
+{
+	//TODO: Cant we set the original pointer to NULL?
+	if(n != NULL)
+	{
+		if(is_leaf(n)){
+			free(n);
+		}
+		else{
+			for(int i = 0; i<n->n_child; i++){
+				deep_free_node(n->children[i]);
+			}
+		}
+
+		//return 1;
+	}
+	return 0;
+}
+
+int height(Node* n)
+{
+	assert(n != NULL);
+	int h = 0;
+	if(n->n_child != 0)
+	{
+		//Code ...
+		for(int i = 0; i<n->n_child; i++){
+			int h_aux = height(n->children[i])+1;
+			if(h_aux > h){
+				h = h_aux;
+			}
+		}
+		return h;
+	}
+	return h + 1;
+}
+
+void uncompile(FILE* outfile, Node *n)
+{
+	if(n != NULL)
+	{
+		//Code ...
+		for(int i = 0; i < n->n_child; i++)
+    {
+        for (int i = 0; i < level; i++)
+            fprintf(outfile, "\t");
+
+        fprintf(outfile ,"Node: %s\n", n->lexeme);
+
+        if (!is_leaf(n->children[i]))
+        {
+            for (int i = 0; i < level; i++)
+                fprintf(outfile, "\t");
+
+            fprintf(outfile, "Children:\n");
+            uncompile(outfile, n->children[i], level + 1);
+        }
+    }
+	}
+}
